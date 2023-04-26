d2689790e9b042230c9d52692ecb428c27922bea
peldonten <peldon.ps10@hotmail.com>
2023-04-26 15:26:29 +0200

    added save csv

--- Exercise3_a.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-
-# Exercise 3 (a)
-
-# Generate position of K+ decay vertex according to exponential distribution
--- Project III.py
+++ Project III.py
@@ -9,68 +9,67 @@ from sklearn.ensemble import AdaBoostClassifier
 import scipy.optimize
 
 
-#Part 3
+# Part 3
 
-#3a)
+# 3 a)
 
+# 3 b)
 
+# 3 c)
 
+c = 299792458           # m/s
 
+l_k = 609.5             # measured mean decay length of kaon
+l_pi = 4188             # measured mean decay length of pion
 
-
-
-#3b)
-
-
-
-
-
-#3c)
-
-c = 299792458 #m/s
-
-l_k = 609.5 #measured mean decay length of kaon
-l_pi = 4188 #measured mean decay length of pion
-
-tau = 2.6033*10**(-8) #s
+tau = 2.6033*10**(-8)   # s
 
 
-mass_kaon = 493.68 #Mev
-mass_pi = 193.57 #Mev
+mass_kaon = 493.68      # Mev
+mass_pi = 193.57        # Mev
 
 
+p_pi= mass_pi*l_pi/(c*tau)  # three momentum of the pion in the Kaon rest frame
 
-
-p_pi= mass_pi*l_pi/(c*tau) #three momentum of the pion in the Kaon rest frame
-
-E_pion = np.sqrt(mass_pi**2 + p_pi**2)
+E_pion = np.sqrt(mass_pi*2 + p_pi*2)
 
 gamma = E_pion/mass_pi
 beta = p_pi/E_pion
 
 
-
 boost = np.array([[gamma, 0, 0, beta*gamma],
-         [0, 1, 0, 0]
-         [0, 0, 1, 0]
+         [0, 1, 0, 0],
+         [0, 0, 1, 0],
          [beta*gamma, 0, 0, gamma]])
 
+
+# col = ["Decay length of K^+", "Angle of pi^+", "Angle of pi^0"]  # columns of my data frame
+
+
+arrangle_data = np.zeros(100000)
+angle_lab_frame_plus = np.zeros(100000)
+angle_lab_frame_neutral = np.zeros(100000)
+arrpos_data = np.zeros(100000)
+
+
 for i in range(100000):
-    angle = np.random.uniform(0, 2 * np.pi)
-    decay_position = np.random.exponential(scale=1/562)
+    arrangle_data[i] = np.random.uniform(0, 2 * np.pi)
+    arrpos_data[i] = np.random.exponential(scale=1/562)
 
-    p_pi_plus = np.array([E_pion,0,p_pi*np.cos(angle),p_pi*np.sin(angle)])
-    p_pi_neutral = np.array([E_pion,0,-p_pi*np.cos(angle),-p_pi*np.sin(angle)])
+    p_pi_plus = np.array([E_pion, 0, p_pi*np.cos(arrangle_data[i]), p_pi*np.sin(arrangle_data[i])])
+    p_pi_neutral = np.array([E_pion, 0, -p_pi*np.cos(arrangle_data[i]), -p_pi*np.sin(arrangle_data[i])])
 
-    p_pi_plus_boosted = np.dot(boost,p_pi_plus)
-    p_pi_neutral_boosted = np.dot(boost,p_pi_neutral)
+    p_pi_plus_boosted = np.dot(boost, p_pi_plus)
+    p_pi_neutral_boosted = np.dot(boost, p_pi_neutral)
 
-    angle_lab_frame_plus = np.tan(p_pi_plus_boosted[2]/p_pi_plus_boosted[3])
-    angle_lab_frame_neutral = np.tan(p_pi_neutral_boosted[2]/p_pi_neutral_boosted[3])
-
-
+    angle_lab_frame_plus[i] = np.arctan(p_pi_plus_boosted[2]/p_pi_plus_boosted[3])
+    angle_lab_frame_neutral[i] = np.arctan(p_pi_neutral_boosted[2]/p_pi_neutral_boosted[3])
 
 
+df = pd.DataFrame({'Decay length of K^+': arrpos_data, 'Angle': arrangle_data, 'Angle lab frame pi_plus':
+                    angle_lab_frame_plus, 'Angle lab frame pi_neutral': angle_lab_frame_neutral})
 
+csv_file = df.to_csv('mydata.csv')
 
+# print(df)
 
